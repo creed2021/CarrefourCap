16a121ffad0cdf4e9aa50dd52cab38b779d20afd
Diego Juiz <dvjuiz@indracompany.com>
2025-12-07 19:38:56 +0000

    Corrección Registros, aprob rechazo y contab y consulta de solic

--- app/consulta-estado-solicitudes/dist/Component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("com.carrefour.consultaestadosolicitudes.Component",{metadata:{manifest:"json"}})});
-//# sourceMappingURL=Component.js.map
--- app/maestro-empleados/dist/Component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("com.carrefour.maestroempleados.Component",{metadata:{manifest:"json"}})});
-//# sourceMappingURL=Component.js.map
